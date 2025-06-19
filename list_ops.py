009f0bc2118fb27c6fd95d85576ff4552bde72b5
Vandana Jain <vandanaj@hotmail.com>
2025-06-19 19:35:53 -0400

    adding list_ops

---
+++ list_ops.py
@@ -0,0 +1,12 @@
+items=["a","b","c","d","e"]
+print(items[2:])
+print(items[2:10])
+print(items[2:2])
+print(items[2:3])
+print(items[2:4])
+print(items[-1:])
+print(items[-1:-4:-2])
+
+list1=[1,3,6]
+list1.extend(items)
+print(list1)
