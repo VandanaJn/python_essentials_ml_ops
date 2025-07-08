552698f3d1b6959456df86c7c744806746c957ef
Vandana Jain <vandanaj@hotmail.com>
2025-07-08 13:18:23 -0400

    file ops

---
+++ file_ops.py
@@ -0,0 +1,12 @@
+try:
+    with open("sample1.txt","a+") as file:
+        file.writelines(["\nthis is a new line","\nthis is a new line"])
+        file.seek(0)
+        print(file.read())
+        file.writelines("\nappended")
+except PermissionError:
+    print("permission issue")
+except IsADirectoryError:
+    print("specifies path a directory")
+except OSError as e:
+    print(f"an os error occoured: {e} ")
