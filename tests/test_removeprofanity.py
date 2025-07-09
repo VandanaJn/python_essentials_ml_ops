98b229c1d7639fe1206761bcddfa829b2c5b7ae6
Vandana Jain <vandanaj@hotmail.com>
2025-07-09 13:47:16 -0400

    Adding TestRemoveProfanity with setup and teardown

---
+++ tests/test_removeprofanity.py
@@ -0,0 +1,21 @@
+from removeprofanity import clean_comments
+class TestRemoveProfanity:
+    def setup_method(self):
+        print("This is setup")
+
+    def teardown_method(self):
+        print("This is teardown")
+
+    def setup_class(cls):
+        print("This is setup_class")
+    
+    def teardown_class(cls):
+        print("This is teardown_class")
+
+    def test_clean_comments_unclean(self):
+        assert clean_comments(["comment1","This is comment3"],["comment3","bad"])\
+        ==["comment1","This is ********"]
+
+    def test_clean_comments2_clean(self):
+        assert clean_comments(["This is comment1","This is comment3"],["comment2","bad"])==\
+            ["This is comment1","This is comment3"]
