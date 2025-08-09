3083767939bd04f6dd1df5a087b615d338e7fb02
Vandana Jain <vandanaj@hotmail.com>
2025-08-09 00:33:21 -0400

    fixing typo.(vj)

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -1,9 +1,9 @@
 from nestedlist import flatten_recursive, flatten
 
-def test_flattern_r():
+def test_flatten_r():
     nested_list = [{"a": "b"}, 1, [2, [3, {4, 5, "text"}], 6], 7, (4, 5)]
     assert [1,2,3,4,5,"text",6,7,4,5]==flatten_recursive(nested=nested_list)
 
-def test_flattern():
+def test_flatten():
     nested_list = [{"a": "b"}, 1, [2, [3, {4, 5, "text"}], 6], 7, (4, 5)]
     assert ['a','b',1,2,3,"text",5,4,6,7,4,5]==flatten(nested=nested_list)
