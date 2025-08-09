5f1f762587f632955ec41afe4e3500895aca4927
Vandana Jain <vandanaj@hotmail.com>
2025-08-09 01:41:45 -0400

    more tests.

---
+++ tests/test_csv_linter.py
@@ -0,0 +1,21 @@
+from csv_linter import raw_header_duplicates, carriage_returns, unnamed_columns, zero_count_columns
+import pandas as pd
+
+def test_raw_header_duplicates():
+    l=raw_header_duplicates("carriage.csv")
+    assert l==['notes']
+
+def test_raw_header_duplicates():
+    df = pd.read_csv("carriage.csv", dtype=str)
+    fields=carriage_returns(df)
+    assert fields==(0, 'notes','Aged in French, Hungarian, and American Oak barrels, this is a big voluptuous wine made in Bordeaux style that will age for years. ,kjhh,abc\r\nAromas of cherry, cedar, cassis, and chocolate, coupled with a velvety finish, give this wine a smooth, mouth filling memory of Cabernet Sauvignon.' )
+
+def test_zero_count_columns():
+    df = pd.read_csv("carriage.csv", dtype=str)
+    l=zero_count_columns(df)
+    assert l==['grape', 'notes.1']
+
+def test_unnamed_columns():
+    df = pd.read_csv("carriage.csv", dtype=str)
+    no=unnamed_columns(df)
+    assert no==1
